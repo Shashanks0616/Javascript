561bf83a82109f86657b8f45f1e60a95801f96ac
Shashanks0616 <80109899+Shashanks0616@users.noreply.github.com>
2021-07-04 09:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+// console.log("hello World!")    to print the hello world in javascript
+
+// How  to declare variable in the Javascript
+
+// the variable should be meaningfull
+// The variable does not contain the hyphen
+// The variable does not start with the Number
+// cannot be the reserved keyword
+
+// let name = 'Shashank' 
+// let lastname = 'Singh';
+// console.log(name);
+// console.log(lastname);
+
+// If you do not reassigned the variable use const otherwise use let:
+
+// let rate = 8;
+// rate = 9;
+// console.log(rate);
+
+// // Types of 
+// 1- Primitive or value TYPES 
+    //  a - String
+    //  b-  Number
+    //  c- bollean
+    //  d- undefined 
+    //  e- null
+    // Some examples are given below in the same order ::
+
+// 2- reference TYPES 
+    // a- Array
+    // b-Object
+    // c- function
+
+// let firstname = "Shashank"; 
+// let age = 20;
+// let num = 20.1;
+// let male = true;
+// let role = undefined;
+// let balance = null;
+
+// Suppose you want to know that the name or age is written in the upper statement is
+// what kind of data type then you have to type only "typeof age or what so ever you
+// want to find in that statement".
+
+// there is only one type of number in javascript which is only number it means there 
+// is no any difference between integer or decimal number.
+
+// reference
+
+// 1- OBJECT 
+
+// let name = "Shashank"; 
+// let age = 20;
+
+// Here in the above example we have to to variable one is name and one is age. Supoose 
+// both the name and the age is belongs to the same person then it is comfortable to print.
+// We have to make an object name person to easy the things as shown below:
+ 
+
+
+// /////////         INITIALIZING THE OBJECT            ////////////////////
+
+
+                 // let person={
+                 //     name : "Shashank",
+                 //     age: 20
+                 // }
+                 // console.log(person)
+
+
+                //  if u want to change the name then there is two process
+
+        //   //   1- Dot Notation\
+            // personal.name='rahul';
+
+            //   2- Bracket Notation
+            // person['name']='Rahul';
+
